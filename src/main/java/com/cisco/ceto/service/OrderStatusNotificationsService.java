82499a2373e6612b5cebb07c0975d5b35ac91837
kumanank <66823325+kumanank@users.noreply.github.com>
2026-03-15 23:20:29 +0530

    feat(SOM-58): Add scaffold for Order Status Notifications

--- src/main/java/com/cisco/ceto/service/OrderStatusNotificationsService.java
+++ src/main/java/com/cisco/ceto/service/OrderStatusNotificationsService.java
@@ -1,39 +1,75 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
-public class OrderStatusNotificationService {
+@RestController
+public class OrderStatusController {
+
+    @Autowired
+    private OrderStatusService orderStatusService;
 
     /**
-     * Sends an email notification to the customer about the order status.
-     *
-     * @param customerEmail the email address of the customer
-     * @param orderStatus   the current status of the order
+     * Endpoint to update order status and trigger notifications
+     * 
+     * @param orderStatusUpdate the order status update information
      */
-    @Async
-    public void sendEmailNotification(String customerEmail, String orderStatus) {
+    @PostMapping("/updateOrderStatus")
+    public void updateOrderStatus(@RequestBody OrderStatusUpdate orderStatusUpdate) {
+        orderStatusService.updateOrderStatus(orderStatusUpdate);
+    }
+}
+
+@Service
+class OrderStatusService {
+
+    /**
+     * Update the order status and send notifications
+     * 
+     * @param orderStatusUpdate the order status update information
+     */
+    public void updateOrderStatus(OrderStatusUpdate orderStatusUpdate) {
+        // TODO: Update order status in the database
+        
+        // TODO: Send email notification
+        sendEmailNotification(orderStatusUpdate);
+        
+        // TODO: Send SMS notification
+        sendSmsNotification(orderStatusUpdate);
+        
+        // TODO: Update customer portal in real-time
+        updateCustomerPortal(orderStatusUpdate);
+    }
+
+    /**
+     * Send email notification to the customer
+     * 
+     * @param orderStatusUpdate the order status update information
+     */
+    private void sendEmailNotification(OrderStatusUpdate orderStatusUpdate) {
         // TODO: Implement email sending logic
     }
 
     /**
-     * Sends an SMS notification to the customer about the order status.
-     *
-     * @param customerPhone the phone number of the customer
-     * @param orderStatus   the current status of the order
+     * Send SMS notification to the customer
+     * 
+     * @param orderStatusUpdate the order status update information
      */
-    @Async
-    public void sendSMSNotification(String customerPhone, String orderStatus) {
+    private void sendSmsNotification(OrderStatusUpdate orderStatusUpdate) {
         // TODO: Implement SMS sending logic
     }
 
     /**
-     * Updates the customer portal with the real-time order status.
-     *
-     * @param customerId    the ID of the customer
-     * @param orderStatus   the current status of the order
+     * Update the customer portal with the new order status
+     * 
+     * @param orderStatusUpdate the order status update information
      */
-    @Async
-    public void updateCustomerPortal(String customerId, String orderStatus) {
-        // TODO: Implement logic to update customer portal
+    private void updateCustomerPortal(OrderStatusUpdate orderStatusUpdate) {
+        // TODO: Implement customer portal update logic
     }
+}
+
+class OrderStatusUpdate {
+    // TODO: Define necessary fields for order status update
 }
