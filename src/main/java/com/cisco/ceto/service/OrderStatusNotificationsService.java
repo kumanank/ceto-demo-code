4f2201bd96ee8e8dfbc51dcb211f43db497b1122
kumanank <66823325+kumanank@users.noreply.github.com>
2026-03-15 18:32:40 +0530

    feat(SOM-58): Add scaffold for Order Status Notifications

--- src/main/java/com/cisco/ceto/service/OrderStatusNotificationsService.java
+++ src/main/java/com/cisco/ceto/service/OrderStatusNotificationsService.java
@@ -1,80 +1,82 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@Service
+public class OrderStatusNotificationService {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private SmsService smsService;
+
+    @Autowired
+    private CustomerPortalService customerPortalService;
+
+    /**
+     * Sends real-time order status updates to customers via email and SMS.
+     *
+     * @param orderId the ID of the order
+     * @param status  the new status of the order
+     */
+    public void sendOrderStatusUpdate(String orderId, String status) {
+        // TODO: Implement logic to send email notification
+        emailService.sendEmailNotification(orderId, status);
+
+        // TODO: Implement logic to send SMS notification
+        smsService.sendSmsNotification(orderId, status);
+
+        // TODO: Implement logic to update customer portal
+        customerPortalService.updateOrderStatus(orderId, status);
+    }
+}
 
 @RestController
-public class OrderStatusController {
+@RequestMapping("/api/orders")
+class OrderStatusController {
 
     @Autowired
-    private OrderStatusService orderStatusService;
+    private OrderStatusNotificationService orderStatusNotificationService;
 
     /**
-     * Endpoint to update order status and trigger notifications
-     * 
-     * @param orderId the ID of the order to update
-     * @param status the new status of the order
-     */
-    @PostMapping("/orders/{orderId}/status")
-    public void updateOrderStatus(@PathVariable String orderId, @RequestBody String status) {
-        orderStatusService.updateOrderStatus(orderId, status);
-    }
-}
-
-@Service
-class OrderStatusService {
-
-    @Autowired
-    private EmailNotificationService emailNotificationService;
-
-    @Autowired
-    private SmsNotificationService smsNotificationService;
-
-    /**
-     * Update the order status and send notifications
-     * 
-     * @param orderId the ID of the order to update
-     * @param status the new status of the order
-     */
-    public void updateOrderStatus(String orderId, String status) {
-        // TODO: Update order status in the database
-        
-        // TODO: Notify customer via email
-        emailNotificationService.sendOrderStatusUpdate(orderId, status);
-        
-        // TODO: Notify customer via SMS
-        smsNotificationService.sendOrderStatusUpdate(orderId, status);
-        
-        // TODO: Update customer portal in real-time
-    }
-}
-
-@Service
-class EmailNotificationService {
-
-    /**
-     * Send order status update via email
-     * 
+     * Endpoint to update order status and trigger notifications.
+     *
      * @param orderId the ID of the order
-     * @param status the new status of the order
+     * @param status  the new status of the order
      */
-    public void sendOrderStatusUpdate(String orderId, String status) {
-        // TODO: Implement email sending logic
+    @PutMapping("/{orderId}/status")
+    public void updateOrderStatus(@PathVariable String orderId, @RequestParam String status) {
+        orderStatusNotificationService.sendOrderStatusUpdate(orderId, status);
     }
 }
 
-@Service
-class SmsNotificationService {
-
+interface EmailService {
     /**
-     * Send order status update via SMS
-     * 
+     * Sends an email notification to the customer.
+     *
      * @param orderId the ID of the order
-     * @param status the new status of the order
+     * @param status  the new status of the order
      */
-    public void sendOrderStatusUpdate(String orderId, String status) {
-        // TODO: Implement SMS sending logic
-    }
+    void sendEmailNotification(String orderId, String status);
+}
+
+interface SmsService {
+    /**
+     * Sends an SMS notification to the customer.
+     *
+     * @param orderId the ID of the order
+     * @param status  the new status of the order
+     */
+    void sendSmsNotification(String orderId, String status);
+}
+
+interface CustomerPortalService {
+    /**
+     * Updates the order status in the customer portal.
+     *
+     * @param orderId the ID of the order
+     * @param status  the new status of the order
+     */
+    void updateOrderStatus(String orderId, String status);
 }
